e40581478583344ab665a45ddac2bbc34381838b
karsh <utkarsh@cyberfear.com>
2025-04-06 17:11:23 +1000

    feat: task handler

--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,19 @@
 
-extern "C" void app_main()
+
+void TaskController( void *pvParameters )
 {
+    TickType_t lStartWakeTime = xTaskGetTickCount();
+    const TickType_t DELAY = pdMS_TO_TICKS( 1 );
+
+    for ( ;; )
+    {
+        vTaskDelayUntil( &lStartWakeTime, DELAY );
+    }
+}
+
+extern "C" void app_main( void )
+{
+    initArduino();
+
+    xTaskCreatePinnedToCore( TaskController, "TaskController", 4096, NULL, 5, NULL, 1 );
 }
