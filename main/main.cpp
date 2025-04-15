734198a0f3b176d43b0dd32b601e4d10ce83c73e
karsh <utkarsh@cyberfear.com>
2025-04-15 11:17:05 +1000

    perf: remove timer task
    * it has been quit redundant if you think about it. setting up delayed
      loop within app_main(); is sufficient

--- main/main.cpp
+++ main/main.cpp
@@ -4,40 +4,34 @@
 #include "ECG.h"
 #include "Motion.h"
 
-ECGSensor ECG;
-MotionSensor Motion;
-BLEHandler Bluetooth;
-float ECGValues[ECG_SAMPLE];
-
-void TaskController( void *pvParameters )
+extern "C" void app_main( void )
 {
-    TickType_t lStartWakeTime = xTaskGetTickCount();
-    const TickType_t DELAY = pdMS_TO_TICKS( 1 );
+    initArduino();
+    Wire.begin( SDA_PIN, SCL_PIN, I2C_RATE );
 
-    for ( ;; )
+    ECGSensor ECG;
+    MotionSensor Motion;
+    BLEHandler Bluetooth;
+
+    uint lECGValues[ECG_SAMPLE];
+
+    const TickType_t DELAY = pdMS_TO_TICKS( 1 );
+    TickType_t lStartWakeTime = xTaskGetTickCount();
+
+    while ( true )
     {
         Action( Bluetooth.Receive() );
 
         for ( int i = 0; i < ECG_SAMPLE; i++ )
         {
             xTaskNotifyGive( ECG.TaskHandler );
-            ECGValues[i] = ECG.Values();
+            lECGValues[i] = ECG.Values();
             vTaskDelayUntil( &lStartWakeTime, DELAY );
         }
+
         xTaskNotifyGive( Motion.TaskHandler );
         vTaskDelayUntil( &lStartWakeTime, DELAY );
 
-        Bluetooth.Send( Motion.Values(), ECGValues );
+        Bluetooth.Send( Motion.Values(), lECGValues );
     }
-}
-
-extern "C" void app_main( void )
-{
-    initArduino();
-    Wire.begin( SDA_PIN, SCL_PIN, I2C_RATE );
-
-    Motion.Init();
-    ECG.Init();
-
-    xTaskCreatePinnedToCore( TaskController, "TaskController", 4096, NULL, 5, NULL, 1 );
 }
