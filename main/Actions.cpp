7922dc01f3aade3d714084045136dc8f5ae6bb68
karsh <utkarsh@cyberfear.com>
2025-04-16 11:37:53 +1000

    feat: NeoPixel control
    + added framework to control onboard led

--- main/Actions.cpp
+++ main/Actions.cpp
@@ -1,5 +1,33 @@
 
 #include "Actions.h"
+#include "Adafruit_NeoPixel.h"
+
+Adafruit_NeoPixel Strip( 1, 48, NEO_GRB + NEO_KHZ800 );
+
+void toggleOnBoardRGB()
+{
+    Strip.begin();
+    Strip.show(); // Initialize all pixels to 'off'
+
+    while (1)
+    {
+        // Red
+        Strip.setPixelColor(0, Strip.Color(255, 0, 0));
+        Strip.show();
+        delay(1000);
+
+        // Green
+        Strip.setPixelColor(0, Strip.Color(0, 255, 0));
+        Strip.show();
+        delay(1000);
+
+        // Blue
+        Strip.setPixelColor(0, Strip.Color(0, 0, 255));
+        Strip.show();
+        delay(1000);
+    }
+}
+
 
 void Action( std::string aData )
 {
