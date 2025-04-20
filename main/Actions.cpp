adb8a0166f8e7fb1ed3dffe1b61edd7c52af94b7
karsh <utkarsh@cyberfear.com>
2025-04-20 19:39:28 +1000

    chore: ActionUpdate() & ActionBeep()
    + added functions to parse JSON input and make hardware changes

--- main/Actions.cpp
+++ main/Actions.cpp
@@ -1,13 +1,24 @@
 
+#include <iostream>
 #include "Actions.h"
 #include "Pins.h"
 #include "Adafruit_NeoPixel.h"
+
+// Keys here should match the edge server JSON generator method's keys
+Actuator ActuatorsList[] = {
+    { .Pin = 10, .Key = "fan",    .Status = STOP, .State = false }, 
+    { .Pin = 12, .Key = "buzzer", .Status = STOP, .State = false }, 
+    { .Pin = 13, .Key = "ledRed", .Status = STOP, .State = false }, 
+    { .Pin = 13, .Key = "ledRGB", .Status = STOP, .State = false }, 
+};
+#define ACTUATORS_COUNT 4
+#define ACTUATOR_I ( &ActuatorsList[0] + i )
 
 Adafruit_NeoPixel Strip( 1, 48, NEO_GRB + NEO_KHZ800 );
 
 //- Private Methods --------------------------------------------------------------------------------
 
-void toggleOnBoardRGB()
+void toggleOnBoardRGB( void )
 {
     Strip.begin();
     Strip.show(); // Initialize all pixels to 'off'
@@ -34,7 +45,38 @@ void toggleOnBoardRGB()
 
 //- Public Methods ---------------------------------------------------------------------------------
 
-void Action( std::string aData )
+void ActionUpdate( string aData )
 {
-    //
+    for ( size_t i = 0; i < ACTUATORS_COUNT; ++i )
+    {
+        const string lSearchKey = string( "\"" ) + ACTUATOR_I->Key + "\":";
+        const size_t lPosition = aJSON.find( lSearchKey );
+
+        if ( lPosition != string::npos )
+        {
+            const size_t lStartIndex = aJSON.find( ':', lPosition ) + 1;
+            size_t lEndIndex = aJSON.find( ',', lStartIndex );
+
+            if ( lEndIndex == string::npos )
+                lEndIndex = aJSON.find( '}', lStartIndex );
+
+            const int lValue = std::stoi( aJSON.substr( lStartIndex, lEndIndex - lStartIndex ));
+
+            if ( lValue != UNCHANGED )
+                ACTUATOR_I->Status = lValue;
+        }
+    }
+}
+
+void ActionBeep( ActionPace aPace )
+{
+    for ( size_t i = 0; i < ACTUATORS_COUNT; ++i )
+    {
+        if ( ACTUATOR_I->Status == aPace )
+        {
+            ACTUATOR_I->State != ACTUATOR_I->State;
+
+            digitalWrite( ACTUATOR_I->Pin, ACTUATOR_I->State );
+        }
+    }
 }
--- main/include/Actions.h
+++ main/include/Actions.h
@@ -2,6 +2,22 @@
 #pragma once
 
 #include <string>
-using std::string;
 
-void Action( string );
+//- Configs ----------------------------------------------------------------------------------------
+
+using std::string;
+typedef unsigned int uint;
+typedef enum { STOP, START, UNCHANGED, BEEP, BEEP_FAST, BEEP_SLOW } AStatus;
+typedef enum { REGULAR = 3, FAST, SLOW } APace;
+
+typedef struct {
+    uint    Pin;
+    string  Key;
+    AStatus Status;
+    bool    State;
+} Actuator;
+
+//- Public Methods ---------------------------------------------------------------------------------
+
+void ActionUpdate( string );
+void ActionBeep( APace );
